09dfb0571f94b71ecf10230656c78809c0d7c5f1
Alexandr Melyakin <melyakin232384@mail.ru>
2022-07-12 14:31:52 +0600

    Mелкие изменения в пользу оптимизации

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+VANTA.FOG({
+    el: "#footer",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0xa5a0ff,
+    midtoneColor: 0xffb4e5,
+    lowlightColor: 0x8cffc8,
+    baseColor: 0xffffff
+})
+VANTA.BIRDS({
+    el: "#about",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    quantity: 4.00
+})
+
+
+
 const menuLinks = document.querySelectorAll('.menu__item[data-goto]');
 if(menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
